7b845357cd6bb7549e1c0f8390cb73f6723f7233
=mysterdeveloper <=manus.duggan@yahoo.com>
2016-09-24 21:04:51 +0100

    Connect to Couchdb with new packages added

--- src/main/main.go
+++ src/main/main.go
@@ -12,7 +12,16 @@ import (
     "log"
     "bufio"
     "strings"
+    "github.com/rhinoman/couchdb-go"
+    "time"
+    "crypto/rand"
+	"fmt"
+	"io"
 )
+type TestDocument struct {
+    Title string
+    Note string
+}
 
 func main(){
     serveWeb()
@@ -22,8 +31,7 @@ var themeName = getThemeName()
 var staticPages = populateStaticPages()
 
 func serveWeb(){
-    
-    gorillaRoute:= mux.NewRouter()
+    gorillaRoute:= mux.NewRouter() 
     
     gorillaRoute.HandleFunc("/", serveContact)
     gorillaRoute.HandleFunc("/{page_alias}", serveContact)
@@ -32,9 +40,33 @@ func serveWeb(){
     http.HandleFunc("/css/", serveResource)
     http.HandleFunc("/js/", serveResource)
     
-    
     http.Handle("/", gorillaRoute)
     http.ListenAndServe(":8080",nil)
+}
+
+func couchdbSave(){
+    var timeout = time.Duration(500 * 100000000)
+    conn, err := couchdb.NewConnection("127.0.0.1",5985,timeout)
+    auth := couchdb.BasicAuth{Username: "Manus", Password: "manus" }
+    db := conn.SelectDB("manus", &auth)
+
+    theDoc := TestDocument{
+        Title: "My Document",
+        Note: "This is a note",
+    }
+
+    theId, err :=newUUID() //use whatever method you like to generate a uuid
+    //The third argument here would be a revision, if you were updating an existing document
+    if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+    rev, err := db.Save(theDoc, theId, "")  
+    //If all is well, rev should contain the revision of the newly created
+    //or updated Document
+    
+    log.Println(rev)
+    log.Println(err)
+    
 }
 
 func serveContact(w http.ResponseWriter, r *http.Request){
@@ -102,4 +134,19 @@ func serveResource(w http.ResponseWriter, req *http.Request){
         w.WriteHeader(404)
     }
     
+}
+
+
+// newUUID generates a random UUID according to RFC 4122
+func newUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 }
