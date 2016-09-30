0dc7073dd74f788f66060436e2eb5da196f7f99a
Ross Byrne <ross.byrne@outlook.ie>
2016-09-30 17:36:38 +0100

    Angular Routing Works Correctly

--- src/main/serveAng.go
+++ src/main/serveAng.go
@@ -1,48 +1,59 @@
 
-// file mux_ex.go
 package main
 
 import (
- //"log"
  "fmt"
  "net/http"
  "strings"
- //"github.com/gorilla/mux"
 )
 
-/*func main() {
-  r := mux.NewRouter()
-  r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("angular/"))))
+/*
+    To get routing to work in the angular app, the index.html file needs to be served
+    everytime a route is entered into the address bar. This lets angular 2 handle the routing
+    instead of Go. In doing this, I ran into the problem of Go returning js files as html.
+    To solve this (after many many hours) I used some code from the answer on this post
+    Link: http://stackoverflow.com/questions/14086063/serve-homepage-and-static-content-from-root
+*/
 
-  http.Handle("/", r)
-
-  log.Println("Listening at port 3000")
-  http.ListenAndServe(":3000", nil)
-
-}*/
-
-
-
+// for serving angular resources (all angular app files needed)
 var chttp = http.NewServeMux()
 
 func main() {
 
-    chttp.Handle("/", http.FileServer(http.Dir("./angular")))
+  // handle for serving resource
+  chttp.Handle("/", http.FileServer(http.Dir("./angular")))
 
-    http.HandleFunc("/", HomeHandler) // homepage
+  // handle serving index.html at root
+  http.HandleFunc("/", homeHandler)
 
-    fmt.Println("Listening on port 8080")
+  // give the user feedback
+  fmt.Println("Listening on port 8080")
 
-    http.ListenAndServe(":8080", nil)
-}
+  // listen on port and handle connections
+  http.ListenAndServe(":8080", nil)
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+} // main()
 
-    if (strings.Contains(r.URL.Path, ".")) {
-        fmt.Println("Say what now?")
-        chttp.ServeHTTP(w, r)
-    } else {
-        http.ServeFile(w, r, "angular/index.html")
-        fmt.Println("Serve index.html")
-    }
-}
+// handler for root address
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+  // if path has a . in it, it is looking for a file
+  if (strings.Contains(r.URL.Path, ".")) {
+
+      // print out required file path
+      fmt.Println("Reqired File Path:", r.URL.Path)
+
+      // serve the required resources using the resource handle declared further up
+      chttp.ServeHTTP(w, r)
+
+  } else { // if path is not for a resource
+
+      // serve the index.html file again, angular 2 is handling the routing
+      http.ServeFile(w, r, "angular/index.html")
+
+      // print out feedback
+      fmt.Println("Serve index.html")
+
+  } // if
+
+} // homeHandler()
