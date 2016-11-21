4b85d642677e5319fccf9aebbdf018737c3eeb64
Martinc94 <martinc94@live.co.uk>
2016-11-21 19:15:05 +0000

    added make a thread

--- bin/angular/app/thread-page.component/thread-page.component.js
+++ bin/angular/app/thread-page.component/thread-page.component.js
@@ -11,23 +11,77 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 // System imports
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
+var thread_1 = require('../classes/thread/thread');
 var thread_service_1 = require('./thread.service');
 var ThreadPageComponent = (function () {
     function ThreadPageComponent(router, threadService) {
         this.router = router;
         this.threadService = threadService;
-        //title = "Threads";
+        this.startIndex = 0;
+        this.threadsPerPage = 5;
         this.threads = [];
+        this.threadTitle = "";
+        this.threadBody = "";
+        this.threadTags = "";
+        this.opened = false;
     }
+    ThreadPageComponent.prototype.toggle = function () {
+        this.opened = !this.opened;
+    };
     ThreadPageComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.threadService.getThreads()
-            .then(function (threads) { return _this.threads = threads.slice(0, 10); });
+            .then(function (threads) { return _this.threads = threads; });
     };
     ThreadPageComponent.prototype.gotoDetail = function (thread) {
         var link = ['/threads', thread.id];
         this.router.navigate(link);
     };
+    ThreadPageComponent.prototype.nextPage = function () {
+        if (this.startIndex < this.threads.length - this.threadsPerPage) {
+            this.startIndex++;
+        } // if;
+    }; // nextPage()
+    ThreadPageComponent.prototype.previousPage = function () {
+        if (this.startIndex > 0) {
+            this.startIndex--;
+        } // if
+    }; // previousPage()
+    ThreadPageComponent.prototype.lastPage = function () {
+        while (this.startIndex < this.threads.length - this.threadsPerPage) {
+            this.startIndex++;
+        } // if;
+    }; // lastPage()
+    ThreadPageComponent.prototype.goToBottomOfPage = function (timeout) {
+        // wait a selected amount of time before scrolling to bottom of page
+        window.setTimeout(function () { document.getElementById('bottomOfPage').scrollIntoView(); }, timeout);
+    }; // goToBottomOfPage()
+    ThreadPageComponent.prototype.goToTopOfPage = function (timeout) {
+        // wait a selected amount of time before scrolling to bottom of page
+        window.setTimeout(function () { document.getElementById('topOfPage').scrollIntoView(); }, timeout);
+    }; // goToTopOfPage()
+    ThreadPageComponent.prototype.saveThread = function (threadTitle, threadBody, threadTags) {
+        //add validation to avoid blank posts
+        var thread = new thread_1.Thread();
+        var splitTags = threadTags.split(",");
+        thread.author = "Martin";
+        thread.title = threadTitle;
+        thread.body = threadBody;
+        thread.tags = splitTags;
+        thread.id = 0;
+        // add the thread to the threads object (this is temp)
+        this.threads.push(thread);
+        // scroll to the bottom of the page (so thread can be seen)
+        this.goToBottomOfPage(10);
+        // try to go to the last page
+        this.lastPage();
+        // save in couchDB
+        this.threadService.saveThread(thread);
+        // clear
+        this.threadTitle = "";
+        this.threadBody = "";
+        this.threadTags = "";
+    }; // saveThread
     ThreadPageComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
@@ -38,5 +92,5 @@ var ThreadPageComponent = (function () {
     ], ThreadPageComponent);
     return ThreadPageComponent;
 }());
-exports.ThreadPageComponent = ThreadPageComponent;
+exports.ThreadPageComponent = ThreadPageComponent; //end ThreadPageComponent
 //# sourceMappingURL=thread-page.component.js.map
