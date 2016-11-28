9cfa55e75e5f9e63d4c3bd88812f2db0a2e5ffa7
Ross Byrne <ross.byrne@outlook.ie>
2016-11-28 15:09:06 +0000

    Updated test data

--- bin/angular/app/test-data/posts-test.js
+++ bin/angular/app/test-data/posts-test.js
@@ -1,3 +1,26 @@
+// fake data source for posts for testing
 "use strict";
-exports.POSTS = [];
+exports.POSTS = [
+    {
+        Posts: [
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Mr. Nice' },
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Narco' },
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Bombasto' }
+        ]
+    },
+    {
+        Posts: [
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Celeritas' },
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Magneta' },
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'RubberMan' }
+        ]
+    },
+    {
+        Posts: [
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Dynama' },
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Dr IQ' },
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Magma' }
+        ]
+    }
+];
 //# sourceMappingURL=posts-test.js.map
--- bin/angular/app/test-data/posts-test.ts
+++ bin/angular/app/test-data/posts-test.ts
@@ -1,32 +1,33 @@
 
 // fake data source for posts for testing
+
 import { Post } from '../classes/post/post';
 import { ThreadPosts } from '../classes/thread-posts/thread-posts';
 
 export const POSTS: ThreadPosts[] = 
  [
-//     {   
-//         Posts: 
-//         [
-//             {id: "", threadPostId: "", body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", authorId: "", authorName: 'Mr. Nice'},
-//             {id: "", threadPostId: "", body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", authorId: "", authorName: 'Narco'},
-//             {id: "", threadPostId: "", body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", authorId: "", authorName: 'Bombasto'}
-//         ]
-//     },
-//     { 
-//         Posts: 
-//         [
-//             {id: "", threadPostId: "", body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", authorId: "", authorName: 'Celeritas'},
-//             {id: "", threadPostId: "", body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", authorId: "", authorName: 'Magneta'},
-//             {id: "", threadPostId: "", body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", authorId: "", authorName: 'RubberMan'}
-//         ]
-//     },
-//     {   
-//         Posts: 
-//         [
-//             {id: "", threadPostId: "", body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", authorId: "", authorName: 'Dynama'},
-//             {id: "", threadPostId: "", body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", authorId: "", authorName: 'Dr IQ'},
-//             {id: "", threadPostId: "", body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", authorId: "", authorName: 'Magma'}
-//         ]
-//     }
+    {   
+        Posts: 
+        [
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Mr. Nice'},
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Narco'},
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Bombasto'}
+        ]
+    },
+    { 
+        Posts: 
+        [
+            {Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Celeritas'},
+            {Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Magneta'},
+            {Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'RubberMan'}
+        ]
+    },
+    {   
+        Posts: 
+        [
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Dynama'},
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Dr IQ'},
+            { Id: "", ThreadPostId: "", Body: "This is the post text. This is the post text. This is the post text. This is the post text. This is the post text. This is the post text.", AuthorName: 'Magma'}
+        ]
+    }
 ];
